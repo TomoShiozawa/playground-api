cac5bfc3aae27134290999bc28be29d3188e9fed
shiotomo <subtle.tomo@gmail.com>
2020-08-28 01:29:00 +0900

    feat: Add endpoint '/dice/:code': dice roll with code.

--- app/controllers/dice/dice_controller.rb
+++ app/controllers/dice/dice_controller.rb
@@ -8,4 +8,28 @@ class DiceController < Sinatra::Base
   get '/dice' do
     { result: rand(1..6) }.to_json
   end
+
+  get '/dice/:code' do
+    code = params[:code]
+
+    unless /\d+d\d+\z/.match?(code)
+      return {
+        result: 'ERROR. The format of code is incorrect.'
+      }.to_json
+    end
+
+    num = code.split('d')[0].to_i
+    sides = code.split('d')[1].to_i
+
+    p [num, sides]
+
+    result = 0
+    num.times do
+      result += (1..sides).to_a.sample
+    end
+
+    {
+      result: result
+    }.to_json
+  end
 end
