cb97c769f75d1e1cdb0bdc6cc1a032e6e1fd387c
shiotomo <subtle.tomo@gmail.com>
2020-09-26 23:05:59 +0900

    feat: Add details to result of combination.

--- app/controllers/combination/combination_controller.rb
+++ app/controllers/combination/combination_controller.rb
@@ -15,8 +15,7 @@ class CombinationController < Sinatra::Base
     n = combination.split(/[cC]/)[0].to_i
     r = combination.split(/[cC]/)[1].to_i
 
-    return { result: 0 }.to_json if n.zero?
-
-    return { result: (1..n).to_a.combination(r).to_a.length }.to_json
+    combinations = (1..n).to_a.combination(r).to_a
+    return { num_of_combinations: combinations.length, combinations: combinations }.to_json
   end
 end
